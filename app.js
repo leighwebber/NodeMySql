aba1779f077ae743dab775252f4e87dea758b055
leighwebber <leighwebber@live.com>
2026-05-13 08:18:46 -0700

    Working - next encrypt

--- app.js
+++ app.js
@@ -3,24 +3,55 @@ const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = 3000;
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 
 // MySQL Connection
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'root',
+  user: 'lwebber',
   password: '12qwQW!!',
-  database: 'crud_api'
+  database: 'playblocker'
 });
 
 db.connect(err => {
   if (err) throw err;
   console.log('✅ MySQL Connected!');
 });
+
+app.get("/", (req, res) => {
+  db.query("select * from users", (err, result, field) => {
+      if(err) {
+        console.log(err);
+      }
+      else {
+        res.send(result);
+      }
+  })
+});
+
+app.post("/register", (req, res) => {
+	console.log(req.body);
+	db.query("insert into users(username, email, password) values('"+req.body.username+"', '"+req.body.email+"', '"+req.body.password+"')", (err, result, field) => {
+		if(err) {
+			console.log(err);
+		}
+		else {
+			console.log ("user successfully registered.");
+			res.send("User successfully registered.");
+		}
+  });
+});
+
+
 
 app.post('/api/users', (req, res) => {
   const { name, email } = req.body;
@@ -41,6 +72,8 @@ app.get('/api/users', (req, res) => {
     res.json(results);
   });
 });
+
+
 
 app.get('/api/users/:id', (req, res) => {
   const { id } = req.params;
